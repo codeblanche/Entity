a7bc7400bead90b07b3a39b36d64ea0364dc3e12
Merten van Gerven <merten.vg@gmail.com>
2012-09-14 21:18:31 +0200

    Fixing the vendor dir

---
+++ vendor/Autoload/Exception/RuntimeException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Autoload\Exception;
+
+/**
+ * @author     Merten van Gerven
+ * @package    Autoload
+ * @subpackage Exception
+ */
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+}
