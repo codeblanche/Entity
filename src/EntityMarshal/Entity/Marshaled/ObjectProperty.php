ffc679a06ce521354c44f1e3f467e23a64bb91fc
Merten van Gerven <merten.vg@gmail.com>
2012-09-16 18:43:38 +0200

    Refactoring continued

---
+++ src/EntityMarshal/Entity/Marshaled/ObjectProperty.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace EntityMarshal;
+
+use ReflectionClass;
+use ReflectionProperty;
+
+/**
+ * @author     Merten van Gerven
+ * @package    EntityMarshal
+ */
+abstract class ObjectPropertyEntityMarshal extends AbstractEntityMarshal implements ObjectPropertyAccessorInterface
+{
+
+    /**
+    * {@inheritdoc}
+    */
+    protected function getDefaultPropertyType()
+    {
+        return 'mixed';
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    protected function getCalledClassName()
+    {
+        return get_called_class();
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    protected function getPropertiesAndTypes()
+    {
+        $properties  = array();
+        $reflection  = new ReflectionClass($this->getCalledClassName());
+        $publicVars  = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+
+        foreach ($publicVars as $publicVar) { /* @var ReflectionProperty $publicVar */
+            $doc       = $publicVar->getDocComment();
+            $key       = $publicVar->getName();
+            $is_static = $publicVar->isStatic();
+
+            if ($is_static) {
+                continue;
+            }
+
+            $properties[$key] = preg_match('/@var\s+([^\s]+)/i', $doc, $matches) ? $matches[1] : null;
+        }
+
+        return $properties;
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    protected function getDefaultValues()
+    {
+        return get_class_vars($this->getCalledClassName());
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function &__get($name)
+    {
+        return $this->get($name);
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function __set($name, $value)
+    {
+        $this->set($name, $value);
+    }
+
+}
