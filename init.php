563ae2f145190d981fa974203ea6ee976e1c140d
Merten van Gerven <merten.vg@gmail.com>
2012-09-20 07:43:47 +0200

    rename samples to examples

--- init.php
+++ init.php
@@ -4,8 +4,8 @@ require __DIR__.'/vendor/Autoload/SimpleAutoloader.php';
 
 Autoload\SimpleAutoloader::getInstance()->addPaths(
     array(
-    __DIR__.'/src',
-    __DIR__.'/vendor',
+        __DIR__.'/src',
+        __DIR__.'/vendor',
     )
 );
 
